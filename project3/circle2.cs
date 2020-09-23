efaf8a55072b29a828d7f5dd01b5eac53dd14d69
Dave Pape <depape@buffalo.edu>
2020-09-23 15:54:17 -0400

    Update circle2.cs

--- project3/circle2.cs
+++ project3/circle2.cs
@@ -5,7 +5,7 @@
 // Assumes that the object is currently on the edge of a circle, and computes an angle tangent to that circle
 // Sets the objects orientation to that angle, and then moves the object "forward" (the "up" vector in this case, since we're working in XY)
 // Note that moving forward will actually cause the object to move slightly off of its current circle, and thus spiral away from the origin (this becomes more obvious at higher speeds)
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,16 +14,16 @@ public class circle2 : MonoBehaviour
 
     public float speed=1f;
 
-	void Start ()
+    void Start ()
         {
-	    }
+        }
 	
-	void Update ()
+    void Update ()
         {
         Vector3 pos = transform.localPosition;
         Vector3 dir = new Vector3(-pos[1], pos[0], 0f);
         float angle = Mathf.Atan2(-dir[0], dir[1]) * Mathf.Rad2Deg;
         transform.localEulerAngles = new Vector3(0f, 0f, angle);
         transform.Translate(Vector3.up * Time.deltaTime * speed);
-	    }
+        }
 }
