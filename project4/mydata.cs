8ccf05a7e0fe51cb071a0697bf535796fe41b790
Dave Pape <depape@buffalo.edu>
2020-10-11 03:02:13 -0400

    new examples

---
+++ project4/mydata.cs
@@ -0,0 +1,35 @@
+﻿// mydata.cs
+// by Dave Pape, 8 Oct 2020
+//
+// A script with some data - variable "value" - that changes when the user hits
+// a fire button.  The purpose of this is to work with "observer.cs", to
+// demonstrate how one script can access another's variables.
+//
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mydata : MonoBehaviour
+{
+    public float value=0;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    if (Input.GetButton("Fire1"))
+        {
+        value += Time.deltaTime * 3000;
+        transform.localScale = new Vector3(2f,2f,2f);
+        }
+    else if (Input.GetButton("Fire2"))
+        {
+        value -= Time.deltaTime * 3000;
+        transform.localScale = new Vector3(0.5f,0.5f,0.5f);
+        }
+    else
+        transform.localScale = new Vector3(1f,1f,1f);
+    }
+}
---
+++ project4/observer.cs
@@ -0,0 +1,33 @@
+﻿// observer.cs
+// by Dave Pape, 8 Oct 2020
+//
+// Example of a script that accesses data from another script.
+// The "objScript" attribute should be assigned to point to an object that
+// has a mydata.cs script attached to it.
+// This script should be attached to a TextMeshPro object, to display the
+// mydata script's "value" variable.
+//
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class observer : MonoBehaviour
+{
+    public mydata objScript;
+    private TextMeshProUGUI textMesh;
+    
+    void Start()
+        {
+        textMesh = GetComponent<TextMeshProUGUI>();
+        }
+
+    void Update()
+        {
+        int t = (int) Time.time;
+        int val = (int) objScript.value;
+        textMesh.text = $"Time: {t}  Value: {val}";
+        if (val > 9000)
+            textMesh.text += " !!!!!!!!";
+        }
+}
