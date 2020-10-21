39a6d6c5619f748cf82173ceb536d004a8145253
Dave Pape <depape@buffalo.edu>
2020-10-21 15:23:27 -0400

    new array examples

---
+++ project3/createCirclingArray.cs
@@ -0,0 +1,39 @@
+﻿// createCirclingArray.cs
+// by Dave Pape, 21 Oct 2020
+//
+// This is a variation of createCirclingChildren.cs
+// Instead of using Unity's scene hierarchy to keep track of the instantiated
+// objects, it keeps the list of objects in an array.
+// This is intended to demonstrate the basics of allocating & filling an
+// array in C#.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createCirclingArray : MonoBehaviour
+{
+
+    public int numberOfChildren = 1;
+    public Object obj;
+    public float speed = 1f;
+    public float radius = 1f;
+    private GameObject[] children;
+
+    void Start ()
+        {
+        children = new GameObject[numberOfChildren];
+        for (int i=0; i < numberOfChildren; i++)
+            children[i] = (GameObject) Instantiate(obj);
+        }
+    
+    void Update ()
+        {
+        for (int i=0; i < children.Length; i++)
+            {
+            Transform child = children[i].transform;
+            float angle = Time.time * speed + 2f*Mathf.PI*i/children.Length;
+            child.localPosition = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0f) * radius;
+            }        
+        }
+}
---
+++ project5/triangles2.cs
@@ -0,0 +1,46 @@
+﻿// triangles2.cs
+// by Dave Pape, 21 Oct 2020
+//
+// Create a Unity mesh of some number of triangles.
+// This is intended primarily to demonstrate allocating and filling in
+// arbitrary-sized arrays in C#.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triangles2 : MonoBehaviour
+{
+    public int numTriangles = 3;
+    public Material material;
+
+    void Start()
+        {
+        if (!(gameObject.GetComponent<MeshFilter>()))
+            gameObject.AddComponent<MeshFilter>();
+        if (!(gameObject.GetComponent<MeshRenderer>()))
+            gameObject.AddComponent<MeshRenderer>();
+        GetComponent<Renderer>().material = material;
+
+        Vector3[] newVertices = new Vector3[numTriangles * 3];
+        for (int i=0; i < numTriangles; i++)
+            {
+            newVertices[i*3] = new Vector3(i,0,0);
+            newVertices[i*3+1] = new Vector3(i+0.5f, 1, 0);
+            newVertices[i*3+2] = new Vector3(i+1,0,0);
+            }
+        int[] newTriangles = new int[numTriangles * 3];
+        for (int i=0; i < numTriangles; i++)
+            {
+            newTriangles[i*3] = i*3;
+            newTriangles[i*3+1] = i*3+1;
+            newTriangles[i*3+2] = i*3+2;
+            }
+
+        Mesh m = GetComponent<MeshFilter>().mesh;
+        m.Clear();
+        m.vertices = newVertices;
+        m.triangles = newTriangles;
+        m.RecalculateNormals();
+        }
+}
